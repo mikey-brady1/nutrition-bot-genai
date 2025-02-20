d9f7d3f481eff1eeed826450bb70409592ed7ede
mikey-brady1 <michael.brady631208@tufts.edu>
2025-02-19 22:54:02 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import requests
 
-response_main = requests.get("https://replace_with_your_web_server_link")
+response_main = requests.get("https://desperate-cassandra-michaelbradytuftsgenai-5e8e56d3.koyeb.app/")
 print('Web Application Response:\n', response_main.text, '\n\n')
 
-
-data = {"text":"tell me about tufts"}
-response_llmproxy = requests.post("https://replace_with_your_web_server_link/query", json=data)
+data = {"text":"Tell me about nutrition"}
+response_llmproxy = requests.post("https://desperate-cassandra-michaelbradytuftsgenai-5e8e56d3.koyeb.app/query", json=data)
 print('LLMProxy Response:\n', response_llmproxy.text)
